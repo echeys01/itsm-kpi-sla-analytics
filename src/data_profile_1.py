472079cec8301f77720fabe86865caabf455ff2d
Eric Cheyssial <echeyssial@csus.edu>
2026-02-13 13:36:50 -0800

    Clean up helper function calls in main().

--- src/data_profile_1.py
+++ src/data_profile_1.py
@@ -19,48 +19,83 @@ def main():
         cur = conn.cursor()
 
     except OSError as os_e: 
-        print({os_e})
+        print(f"File-related error encountered: {os_e}")
         if conn is None:
             return conn
 
     except sqlite3.Error as sql_e:
-        print({sql_e})
+        print(f"Database connection/config error encountered: {sql_e}")
         if conn is None:
             return conn
+    # try: ...
 
-    csv_change_data = "data/raw/Detail_Change.csv"
-    load_csv_data(csv_change_data, conn)
+    # Load raw data first by number of CSV source files.
+    incidents_csv = "data/raw/Detail_Incident.csv"
+    load_incident_data(incidents_csv, conn)
 
     incident_activity_csv = "data/raw/Detail_Incident_Activity.csv"
-    incidents_csv = "data/raw/Detail_Incident.csv"
+    load_incident_activity_data(incident_activity_csv, conn)
+
     interactions_csv = "data/raw/Detail_Interaction.csv"
+    load_interaction_data(interactions_csv, conn)
+
+    csv_change_data = "data/raw/Detail_Change.csv"
+    load_change_data(csv_change_data, conn)
 
     conn.close() # type: ignore    
 # end main
 
 
-def load_csv_data(csv_data, conn) -> None:
-    df = pd.read_csv(csv_data, sep=';') # Create DataFrame (df) from ITSM change data.
+def load_incident_data(incidents_csv, conn) -> None:
+    y = 1 # placeholder
+    df = pd.read_csv(incidents_csv, sep=';')
+# end load_incident_data
+
+
+def load_incident_activity_data(incident_activity_csv, conn) -> None:
+    df = pd.read_csv(incident_activity_csv, sep=';')
+# end load_incident_activity_data    
+
+
+def load_interaction_data(interactions_csv, conn) -> None:
+    pd.read_csv(interactions_csv, sep=';')
+# end load_interaction_data
     
-    # Obtain shape of DataFrame.
+
+def load_change_data(csv_change_data, conn) -> None:
+    df = pd.read_csv(csv_change_data, sep=';') # Create DataFrame (df) from ITSM change data.
+    
+    # Obtain shape of DataFrame (rows x cols).
     get_df_shape(df)
 
     df.info()
     print(df.isna().sum())
 
-    print(standardize_columns(df))
-    
-# end load_csv_data
-    
+    print(standardize_columns(df)) # Ensure columns are lowercase, have underscore.
 
-# def load_incident_activity()
+    df.to_sql(
+        "raw_changes",
+        conn,
+        if_exists="replace",
+        index=False,
+        chunksize=5000,
+        method="multi"
+    ) # Create separate table to hold initial raw data.
 
-# def load_incident_data()
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS incidents (
+        
+        
+        
+        )
+        """
+    )
+              
+# end load_change_data
 
-# def load_interaction_data() 
 
-
-def get_df_shape(df) -> None:
+def get_df_shape(df) -> None: # Basic helper function to check df dimensions.
     row_count = df.shape[0]
     col_count = df.shape[1]
 
