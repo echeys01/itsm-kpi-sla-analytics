eabddd2a1f46c0a2258f54c164f340405ef3222d
Eric Cheyssial <echeyssial@csus.edu>
2026-03-02 01:25:55 -0800

    Add aggregate and count queries to main KPI query file.

---
+++ sql/itsm_db_kpis.sql
@@ -0,0 +1,9 @@
+-- database: C:\AnalyticsProjects\ITAnalysisProjects\P2-ServiceDeskKPIAnalysis\output\itsm_bpi2014.db
+-- Main query file for assessing predetermined ITSM KPIs.
+
+-- Incident KPIs: These measure incident volume, incident types, open rates, and activity events per incident.
+
+
+
+
+
