da498765deb52d96b933c44966ab6224da8db158
Eric Cheyssial <echeyssial@csus.edu>
2026-03-02 01:29:09 -0800

    Modify aggregate and count queries in main KPI query file.

--- sql/itsm_db_kpis.sql
+++ sql/itsm_db_kpis.sql
@@ -2,8 +2,33 @@
 -- Main query file for assessing predetermined ITSM KPIs.
 
 -- Incident KPIs: These measure incident volume, incident types, open rates, and activity events per incident.
+SELECT DISTINCT priority 
+FROM incidents
+ORDER BY priority;
 
+-- Incident count by configuration item type (in descending order).
+SELECT ci_type, COUNT(*) AS incident_count
+FROM incidents
+GROUP BY ci_type
+ORDER BY incident_count DESC;
 
+--Incident count by configuration type and priority.
+SELECT ci_type, priority, COUNT(*) AS incident_count
+FROM incidents
+GROUP BY priority, ci_type
+ORDER BY incident_count DESC;
 
+-- Find average number of activity events per incident.
+SELECT AVG(number_of_incidents) AS avg_events_per_incident
+FROM (
+    SELECT incident_id, COUNT(*) AS number_of incidents
+    FROM incident_activity
+    GROUP BY incident_id
+);
+
+-- Assess ITSM changes by CAB approval.
+SELECT cab_approval_needed, COUNT(*) as itsm_change_count
+FROM itsm_changes
+GROUP BY cab_approval_needed
 
 
